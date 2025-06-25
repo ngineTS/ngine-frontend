63cc83726f8018cb93cc987a70caff64b2a9aec2
Childish <cedric.belin1@gmail.com>
2025-06-25 19:38:22 +0200

    call first backend api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { environment } from "../environments/environment";
+
 
 @Component({
   selector: 'app-root',
@@ -7,6 +10,14 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  constructor(private _http: HttpClient){}
+
+  ngOnInit(){
+    this._http.get(`${environment.APIURL}navigation`).subscribe(resp => console.log(resp));
+  }
+
+
   title = 'my-app-frontend';
 }
