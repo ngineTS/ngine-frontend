30868bfc3ba48b7dc3b36dcae125c89e9f2108bd
Childish <cedric.belin1@gmail.com>
2025-07-15 13:08:53 +0200

    put components inside ngFor in generic component

--- src/app/core/components/generic/generic.component.ts
+++ src/app/core/components/generic/generic.component.ts
@@ -1,12 +1,13 @@
-import { AfterViewInit, Component, inject, Injector, inputBinding, OnInit, Type, ViewChild, ViewContainerRef } from '@angular/core';
+import { AfterViewInit, Component, inject, Injector, inputBinding, OnInit, Query, QueryList, Type, ViewChild, ViewChildren, ViewContainerRef } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { Navigation } from '../../models/navigation.interface';
 import { GenericService } from '../../services/generic.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-generic',
-  imports: [MatTooltipModule],
+  imports: [MatTooltipModule, CommonModule],
   templateUrl: './generic.component.html',
   styleUrl: './generic.component.scss'
 })
@@ -15,27 +16,27 @@ export class GenericComponent implements OnInit, AfterViewInit {
   constructor(private _route: ActivatedRoute,
               private genericService: GenericService) {}
 
-  @ViewChild('container', { read: ViewContainerRef }) container!: ViewContainerRef;
+  @ViewChildren('container', { read: ViewContainerRef }) container!: QueryList<ViewContainerRef>;
   injector = inject(Injector);
+  navigations!: Navigation[];
 
   ngOnInit() {
+    this.navigations = this._route.snapshot.data["navigations"];
   }
 
   ngAfterViewInit() {
     this.loadComponents();
   }
 
-  async loadComponents(){
-    if (this._route.snapshot.data["navigations"]) {
-      for (const navigation of this._route.snapshot.data["navigations"] as Navigation[]) {
-        const component = await this.genericService.componentStore[navigation.navigationType.name]()
-                          .then(m => m[this.kebabCasetoPascaleCase(navigation.navigationType.name) + 'Component']);
-        const containerRef = this.container.createComponent(component, {
-          injector: this.injector
-        });
-        containerRef.setInput('navigation', navigation);
-      }
-    }
+  async loadComponents() {
+    this.container.map(async (vcr: ViewContainerRef, index: number) => {
+      const component = await this.genericService.componentStore[this.navigations[index].navigationType.name]()
+        .then(m => m[this.kebabCasetoPascaleCase(this.navigations[index].navigationType.name) + 'Component']);
+      const containerRef = vcr.createComponent(component, {
+        injector: this.injector
+      });  
+      containerRef.setInput('navigation', this.navigations[index]);
+    });
   }
 
   kebabCasetoPascaleCase(input: string) {
