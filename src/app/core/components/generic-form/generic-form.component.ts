5d3606a5415b8fd0cd0410c54ec37c6d3206f8f4
Childish <cedric.belin1@gmail.com>
2025-08-02 22:11:35 +0200

    finalize html template

--- src/app/core/components/generic-form/generic-form.component.ts
+++ src/app/core/components/generic-form/generic-form.component.ts
@@ -53,7 +53,7 @@ export class GenericFormComponent<T extends Record<string, any> & { length?: nev
 
     for (const [key, value] of Object.entries(data)) {
       if (this.isStandardInput(value) || this.isDropdownInput(value)) {
-        // It's a form control configuration - create FormControl with the value
+        // It's an input configuration - create FormControl with the value
         group[key] = new FormControl(value.value, value.validators ?? []);
       } else {
         // It's an object - create FormGroup
@@ -74,6 +74,10 @@ export class GenericFormComponent<T extends Record<string, any> & { length?: nev
 
   getFormGroup(formGroup: FormGroup, name: string) {
     return formGroup.get(name) as FormGroup;
+  }
+
+  getFormControl(formGroup: FormGroup, name: string) {
+    return formGroup.get(name) as FormControl;
   }
 
 }
