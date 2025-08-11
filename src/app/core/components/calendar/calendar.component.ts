d340c6e7f4e3aca809e90cc15388e8b42eb3f128
Childish <cedric.belin1@gmail.com>
2025-08-11 18:47:59 +0200

    implement edit calendar event method

--- src/app/core/components/calendar/calendar.component.ts
+++ src/app/core/components/calendar/calendar.component.ts
@@ -1,7 +1,7 @@
 import { CommonModule, DatePipe } from '@angular/common';
 import { Component, Input, SimpleChanges, ViewChild } from '@angular/core';
 import { FullCalendarComponent, FullCalendarModule } from '@fullcalendar/angular';
-import { CalendarApi, CalendarOptions, DateSelectArg } from '@fullcalendar/core';
+import { CalendarApi, CalendarOptions, DateSelectArg, EventClickArg } from '@fullcalendar/core';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import interactionPlugin from '@fullcalendar/interaction';
 import { Calendar, CalendarPayload } from '../../models/calendar.interface'
@@ -31,6 +31,7 @@ export class CalendarComponent {
 
   @Input() navigation!: Navigation;
   @Input() componentSize!: ComponentSize;
+  canEdit = true;
 
   @ViewChild('myFullCalendar') myFullCalendar!: FullCalendarComponent;
   calendarApi!: CalendarApi;
@@ -39,9 +40,10 @@ export class CalendarComponent {
     plugins: [dayGridPlugin, interactionPlugin],
     events: [],
     selectable: true,
-    select: (arg: DateSelectArg) => this.handleDateSelection(arg)
+    select: (arg: DateSelectArg) => this.handleDateSelection(arg),
+    eventClick: (arg: EventClickArg) => this.handleEventClick(arg)
   };
-  
+
   ngOnInit() {
     this.getCalendarEvent();
   }
@@ -70,7 +72,9 @@ export class CalendarComponent {
                       end: event.allDay ? this._datePipe.transform(event.endDate, 'yyyy-MM-dd') ?? undefined : event.endDate,
                       url: event.url,
                       extendedProps: {
-                        description: event.description
+                        description: event.description,
+                        category: event.category,
+                        allDay: event.allDay
                       }
                     });
                   });
@@ -136,6 +140,64 @@ export class CalendarComponent {
         }
       }
     );
+  }
+
+  handleEventClick(arg: EventClickArg) {
+    if(this.canEdit) {
+
+      arg.jsEvent.preventDefault(); // don't let the browser navigate
+      const calendarForm: DeepFormConfig<CalendarPayload> = {
+        startDate: {
+          value: arg.event.start,
+          type: 'date-and-time',
+          validators: [Validators.required]
+        },
+        endDate: {
+          value:  arg.event.end,
+          type: 'date-and-time',
+          validators: [Validators.required]
+        },
+        description: {
+          value: arg.event.extendedProps["description"],
+          type: 'textarea',
+          validators: []
+        },
+        title: {
+          value: arg.event.title,
+          type: 'text',
+          validators: [Validators.required]
+        },
+        category: {
+          value: arg.event.extendedProps["category"],
+          type: 'text',
+          validators: []
+        },
+        url: {
+          value: arg.event.url,
+          type: 'text',
+          validators: []
+        },
+        allDay: {
+          value: arg.event.extendedProps["allDay"],
+          type: 'checkbox',
+          validators: []
+        }
+      }
+
+      this._matDialog.open(
+        GenericFormComponent<CalendarPayload>,
+        { 
+          maxWidth: '700px',
+          data: {
+            formConfig: calendarForm,
+            id: null,
+            navigationId: this.navigation.id,
+            controllerName: 'calendar',
+          }
+        }
+      );
+
+    }
   }
 
 
