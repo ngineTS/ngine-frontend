1b1237f10d90c06fc8148f1851d25817fcde0306
Childish <cedric.belin1@gmail.com>
2025-09-27 17:47:48 +0200

    add description of methos

--- src/app/core/components/content-management/content-management.component.ts
+++ src/app/core/components/content-management/content-management.component.ts
@@ -21,14 +21,23 @@ export class ContentManagementComponent implements OnInit {
               private _matDialog: MatDialog,
               private _snackbarService: SnackBarService) {}
   
-  @Input() navigation!: Navigation;
-  content!: Array<object> | null;
-  tableConfig!: TableViz;
+  @Input() navigation!: Navigation; //the component instance navigation
+  content!: Array<object> | null; //the content reprensenting the table sample
+  tableConfig!: TableViz; //the table and columns/inputs configuration 
 
   ngOnInit() {
     this.getContentInformation();
   }
 
+  /**
+   * Get table and columns configuration for given navigation.
+   * 
+   * If no configuration exist then open configuration form to create new table.
+   * 
+   * Else return table content by calling auto-genereated table API.
+   * 
+   * @returns The table content of unknown type.
+   */
   getContentInformation() {
     this._http.get<TableViz>(`${environment.APIURL}table-viz/navigation/${this.navigation.id}`)
       .pipe(
@@ -60,6 +69,9 @@ export class ContentManagementComponent implements OnInit {
       .subscribe(resp => this.content = resp);
   }
 
+  /**
+   * Methods called when child (generic table) emits Output event after content modification 
+   */
   onContentChange() {
     this.getContentInformation();
   }
