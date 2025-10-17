a1109247f1853a9b22ab82a989e14ce262398b3f
Childish <cedric.belin1@gmail.com>
2025-10-17 15:04:35 +0200

    clean password recovery component

--- src/app/core/auth/components/password-recovery/password-recovery.component.ts
+++ src/app/core/auth/components/password-recovery/password-recovery.component.ts
@@ -4,43 +4,40 @@ import { AuthService } from '../../services/auth.service';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { FormsModule } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
+import { SnackBarService } from '../../../services/snackbar.service';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-password-recovery',
   imports: [
     MatFormFieldModule,
     FormsModule,
-    MatInputModule
+    MatInputModule,
+    MatButtonModule
   ],
   templateUrl: './password-recovery.component.html',
   styleUrls: ['./password-recovery.component.scss'],
 })
-export class PasswordRecoveryComponent implements OnInit {
+export class PasswordRecoveryComponent {
 
   newPassword: string = '';
   repeatPassword: string = '';
   passwordTooShort: boolean = false;
-  isResetPasswordButtonDisabled: boolean = false;
-  successMessage: string | null = null;
   expiredLinkMessage: string | null = null;
 
   constructor(private _authService: AuthService,
               @Inject(MAT_DIALOG_DATA)
-              public data: { token: string }
+              public data: { token: string },
+              private _snackbarService: SnackBarService
              ) { }
 
-  ngOnInit(): void {
-  }
 
-  onPasswordChange(){
-    if(this.newPassword.length >= 8){
-      this.passwordTooShort = false;
-    }
+  onPasswordChange() {
+    this.passwordTooShort = this.newPassword.length < 8 ? true : false;
   }
 
   isResetPasswordDisabled(){
     if(this.newPassword !== this.repeatPassword 
-       || this.isResetPasswordButtonDisabled
        || this.passwordTooShort
        || !this.newPassword){
         return true;
@@ -49,27 +46,23 @@ export class PasswordRecoveryComponent implements OnInit {
   }
 
   onResetPasswordClick(){
-    if(this.newPassword.length < 8){
-      this.passwordTooShort = true;
-    }
-    else{
-      this.isResetPasswordButtonDisabled = true;
-      this._authService.resetUserPassword({
-        newPassword: this.newPassword,
-        repeatPassword: this.repeatPassword,
-        token: this.data.token
-      }).subscribe((resp: any) => {
-        if(resp["affected"] > 0){
-          this.successMessage = "Password reset successfull!"
-          this.expiredLinkMessage = null;
-        }
-        else{
-          this.successMessage = null;
-          this.expiredLinkMessage = resp.toString();
-        }
-      });
-    }
+    this._authService.resetUserPassword({
+      newPassword: this.newPassword,
+      repeatPassword: this.repeatPassword,
+      token: this.data.token
+    }).subscribe((resp: any) => {
+      if(resp["affected"] > 0){
+        this._snackbarService.showSuccessSnackBar('Password reset successfull!');
+        this.expiredLinkMessage = null;
+        this.newPassword = '';
+        this.repeatPassword = '';
+      }
+      else{
+        this.expiredLinkMessage = resp.toString();
+        this.newPassword = '';
+        this.repeatPassword = '';
+      }
+    });
   }
-
 
 }
