4b037caf06a255c340421094e836914a222ff8ab
Childish <cedric.belin1@gmail.com>
2025-10-23 21:34:16 +0200

    implement role management html and style

--- src/app/pages/admin/user-role-management/role-management/role-management.component.ts
+++ src/app/pages/admin/user-role-management/role-management/role-management.component.ts
@@ -1,23 +1,65 @@
 import { Component, OnInit } from '@angular/core';
 import { RoleService } from '../../../../core/services/role.service';
 import { Role } from '../../../../core/models/role.interface';
-import { Observable } from 'rxjs';
+import { retry, take } from 'rxjs';
 import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+
 
 @Component({
   selector: 'app-role-management',
-  imports: [CommonModule],
+  imports: [
+    CommonModule, 
+    MatButtonModule,
+    MatTooltipModule,
+    MatSlideToggleModule,
+    MatFormFieldModule,
+    MatInputModule
+  ],
   templateUrl: './role-management.component.html',
   styleUrl: './role-management.component.scss'
 })
 export class RoleManagementComponent implements OnInit {
 
-  roles!: Observable<Array<Role>>;
+  filteredRoles!: Array<Role>;
+  roles!: Array<Role>;
 
   constructor(private _roleService: RoleService) { }
 
   ngOnInit() {
-    this.roles = this._roleService.getAllRoles();
+    this._roleService.getAllRoles().pipe(retry(2), take(1))
+      .subscribe(resp => {
+        this.roles = resp;
+        this.filteredRoles = resp;
+      });
+  }
+
+  addRole() {
+    console.log('add role');
+  }
+
+  editRole(role: Role) {
+    console.log(role)
+  }
+
+  deleteRole(roleId: string) {
+    console.log(roleId);
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value.trim().toLocaleLowerCase();
+    if (filterValue === '') {
+      this.filteredRoles = this.roles;
+    }
+    else {
+      this.filteredRoles = this.roles.filter(obj => 
+        obj.displayLabel.trim().toLocaleLowerCase().includes(filterValue)
+      );
+    }
   }
 
 }
