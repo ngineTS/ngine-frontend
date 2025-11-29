3ce105529aad4585b39a9c7dd347523624358aa0
Childish <cedric.belin1@gmail.com>
2025-11-30 00:59:12 +0100

    start to implement file management component

--- src/app/core/services/media.service.ts
+++ src/app/core/services/media.service.ts
@@ -12,7 +12,7 @@ export class MediaService {
   baseURL = environment.APIURL;
   constructor(private _http: HttpClient) {}
 
-  getS3ObjectSignedUrl(fileId: string) {
+  getS3ObjectSignedUrl(fileId: string): Observable<string> {
     return this._http.get<string>(`${this.baseURL}file-management/${fileId}`)
       .pipe(
         retry(2),
@@ -20,15 +20,15 @@ export class MediaService {
       );
   }
 
-  getAllMedias() {
-    return this._http.get<Array<Media>>(`${this.baseURL}media-library`)
+  getAllMedias(): Observable<Array<Media>> {
+    return this._http.get<Array<Media>>(`${this.baseURL}media`)
       .pipe(
         retry(2),
         take(1)
       );
   }
 
-  uploadFileToS3(formData: FormData) {
+  uploadFileToS3(formData: FormData): Observable<Media> {
     return this._http.post<Media>(`${this.baseURL}file-management/upload`, formData)
       .pipe(
         retry(2),
--- src/app/pages/admin/media-library/media-library.component.ts
+++ src/app/pages/admin/media-library/media-library.component.ts
@@ -1,11 +1,30 @@
 import { Component } from '@angular/core';
+import { MediaService } from '../../../core/services/media.service';
+import { Media } from '../../../core/models/media.interface';
+import { CommonModule } from '@angular/common';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-media-library',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './media-library.component.html',
   styleUrl: './media-library.component.scss'
 })
 export class MediaLibraryComponent {
+
+  files: Array<Media> = [];
+  fileIdS3UrlMap: Record<string, Observable<string>> = {};
+
+  constructor(public _mediaService: MediaService) { }
+
+  ngOnInit() {
+    this._mediaService.getAllMedias().subscribe(resp => this.files = resp);
+  }
+
+  onDefaultImageMouseEnter(fileName: string) {
+    if (!this.fileIdS3UrlMap[fileName]) {
+      this.fileIdS3UrlMap[fileName] = this._mediaService.getS3ObjectSignedUrl(fileName);
+    }
+  }
 
 }
