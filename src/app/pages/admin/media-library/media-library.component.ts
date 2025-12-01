a8e1e85befccf9b083341acbc70ffdff20888b8d
Childish <cedric.belin1@gmail.com>
2025-12-01 18:10:20 +0100

    implement search bar

--- src/app/pages/admin/media-library/media-library.component.ts
+++ src/app/pages/admin/media-library/media-library.component.ts
@@ -5,16 +5,24 @@ import { CommonModule } from '@angular/common';
 import { Observable } from 'rxjs';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { SnackBarService } from '../../../core/services/snackbar.service';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-media-library',
-  imports: [CommonModule, MatTooltipModule],
+  imports: [
+    CommonModule,
+    MatTooltipModule,
+    MatFormFieldModule,
+    MatInputModule
+  ],
   templateUrl: './media-library.component.html',
   styleUrl: './media-library.component.scss'
 })
 export class MediaLibraryComponent {
 
   files: Array<Media> = [];
+  filteredFiles: Array<Media> = [];
   fileIdS3UrlMap: Record<string, Observable<string>> = {};
 
   constructor(private _mediaService: MediaService,
@@ -22,7 +30,10 @@ export class MediaLibraryComponent {
   ) { }
 
   ngOnInit() {
-    this._mediaService.getAllMedias().subscribe(resp => this.files = resp);
+    this._mediaService.getAllMedias().subscribe(resp => {
+      this.files = resp;
+      this.filteredFiles = resp;
+    });
   }
 
   onDefaultImageMouseEnter(fileName: string) {
@@ -41,6 +52,22 @@ export class MediaLibraryComponent {
         this._snackbarService.showSuccessSnackBar(`Media ${media.name} has been deleted successfully.`);
         this.files = this.files.filter(obj => obj.id !== media.id);
       });
+    }
+  }
+
+  /**
+   * Apply filter on file name on search input keyup.
+   * @param event The keyup event.
+   */
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value.trim().toLocaleLowerCase();
+    if (filterValue === '') {
+      this.filteredFiles = this.files;
+    }
+    else {
+      this.filteredFiles = this.files.filter(obj => 
+        obj.name.trim().toLocaleLowerCase().includes(filterValue)
+      );
     }
   }
 
