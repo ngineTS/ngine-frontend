e4c908d98c35c6533b62581e84df43a607506334
Childish <cedric.belin1@gmail.com>
2026-02-07 21:57:35 +0100

    format

--- src/app/core/services/media.service.ts
+++ src/app/core/services/media.service.ts
@@ -12,42 +12,56 @@ export class MediaService {
   baseURL = environment.APIURL;
   constructor(private _http: HttpClient) {}
 
+  /**
+   * Get S3 file temporary url.
+   * 
+   * @param fileName The file key.
+   * @returns S3 File temporary URL.
+   */
   getS3ObjectSignedUrl(fileName: string): Observable<string> {
     return this._http.get<string>(`${this.baseURL}file-management/${fileName}`)
-      .pipe(
-        retry(2),
-        take(1)
-      );
+      .pipe(retry(1), take(1));
   }
 
+  /**
+   * Get all media metadata.
+   * 
+   * @param orderBy The media property used to order the array.
+   * @param order 'ASC' or 'DESC'.
+   * @returns An obeservable of media array.
+   */
   getAllMedias(orderBy?: keyof Media, order?: 'ASC' | 'DESC'): Observable<Array<Media>> {
     if(orderBy && order) {
       const params = new HttpParams()
         .set('orderBy', orderBy)
         .set('order', order);
       return this._http.get<Array<Media>>(`${this.baseURL}media`, { params })
-      .pipe(
-        retry(2),
-        take(1)
-      );
+        .pipe(retry(1), take(1));
     }
     return this._http.get<Array<Media>>(`${this.baseURL}media`)
       .pipe(retry(1), take(1));
   }
 
+  /**
+   * Upload file to S3.
+   * 
+   * @param formData The file.
+   * @returns The media metadata.
+   */
   uploadFileToS3(formData: FormData): Observable<Media> {
     return this._http.post<Media>(`${this.baseURL}file-management/upload`, formData)
-      .pipe(
-        take(1)
-      );
+      .pipe(take(1));
   }
 
+  /**
+   * Delete media from S3.
+   * 
+   * @param fileName The file key.
+   * @returns A delete response.
+   */
   deleteMedia(fileName: string) {
     return this._http.delete(`${this.baseURL}file-management/${fileName}`)
-      .pipe(
-        retry(2),
-        take(1)
-      );
+      .pipe(retry(1), take(1));
   }
 
 }
