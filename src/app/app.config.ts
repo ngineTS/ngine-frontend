51c5256bf2d3f6b3844ceb3032926df120c989f7
Childish <cedric.belin1@gmail.com>
2026-04-15 17:06:41 +0200

    format

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,7 +4,6 @@ import { routes } from './app.routes';
 import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
 import { tokenInterceptior } from './core/auth/interceptors/auth.interceptor';
 import { provideAnimations } from '@angular/platform-browser/animations';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
 
 export const appConfig: ApplicationConfig = {
